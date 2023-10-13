357d68150eb651915884469b5a58659cfc2c0da2
v-geetheswar <geetheswarvedachalam@gmail.com>
2023-10-13 18:01:23 +0530

    Added Token and TokenType

---
+++ src/evaluator/tokens.py
@@ -0,0 +1,53 @@
+from enum import Enum
+from typing import Type
+
+
+# Define an enumeration to represent different token types.
+class TokenType(Enum):
+    NUMBER = 0
+    OPERATOR = 1
+    FUNCTION = 2
+    PARENTHESIS = 3
+
+
+class Token:
+    """
+    Represents a token in a mathematical expression.
+
+    Args:
+        token_type (TokenType): The type of the token, which can be NUMBER, OPERATOR, FUNCTION, or PARENTHESIS.
+        value (any): The value of the token, which is the actual symbol or text representing the token.
+    """
+
+    def __init__(self, token_type: TokenType, value):
+        self.token_type = token_type.value
+        self.value = value
+
+    def __eq__(self, other):
+        """
+        Compare the type of this token with another token or TokenType.
+
+        Args:
+            other: The token or TokenType to compare with.
+
+        Returns:
+            bool: True if the types match, False otherwise.
+        """
+
+        if isinstance(other, Token):
+            return self.token_type == other.token_type
+        if isinstance(other, TokenType):
+            return self.token_type == other.value
+        return self.token_type == other
+
+
+if __name__ == '__main__':
+    a = Token(TokenType.NUMBER, 0)
+    b = Token(TokenType.NUMBER, 5)
+    if a == TokenType.NUMBER:
+        print(True)
+    if a == b:
+        print(True)
+    # 1 -> OPERATOR
+    if a != 1:
+        print(False)
